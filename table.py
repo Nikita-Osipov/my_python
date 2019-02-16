1b87c42c7bf16fe964e0052e8121385bfd9e596e
Nikita Osipov <nikitaovspb@gmail.com>
2019-02-16 13:52:29 +0300

    init commit

---
+++ table.py
@@ -0,0 +1,16 @@
+atmn=input('Введите номер элемента n: ')
+if atmn:
+    n = float(atmn)
+    if n == 3.0:
+        print('Литий')
+    elif n == 25:
+        print('Марганец')
+    elif n == 80:
+        print('Ртуть')
+    elif n == 17:
+        print('Хлор')
+    else:
+        print('Что это?')
+else:
+    print('Введите значение n')
+        
